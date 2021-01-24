be53a41e48b5290155a0f3faaa2a25628932d55e
batikansosun <batikansosun@gmail.com>
2021-01-24 22:31:31 +0300

    Initial Commit

---
+++ NetGuard/NetGuard.h
@@ -0,0 +1,18 @@
+//
+//  NetGuard.h
+//  NetGuard
+//
+//  Created by Batıkan SOSUN on 24.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetGuard.
+FOUNDATION_EXPORT double NetGuardVersionNumber;
+
+//! Project version string for NetGuard.
+FOUNDATION_EXPORT const unsigned char NetGuardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetGuard/PublicHeader.h>
+
+
