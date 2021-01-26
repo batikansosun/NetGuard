b3f5688c266336a0158470219b29b3cb54e1a139
batikansosun <batikansosun@gmail.com>
2021-01-27 00:17:16 +0300

    added README.md

--- NetGuardDemo/AppDelegate.swift
+++ NetGuardDemo/AppDelegate.swift
@@ -14,9 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NetGuard().loadNetGuard()
-        NetGuard.shakeEnabled = false
-        NetGuard().showNetGuard()
-        NetGuard.blackListHosts = ["example.com"]
         return true
     }
 
